5b91274fa67bd8ac57c368c151aff54d4bf4b0af
Polkadoty <andrewdoty1235@gmail.com>
2024-10-23 22:30:33 -0500

    Updating main without image caching

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,9 @@ const nextConfig = {
         hostname: 'api.swarmada.wiki',
       },
     ],
+    minimumCacheTTL: 0,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
 
