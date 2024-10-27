fb8ad3ded7cee210e84ce26f75274e1ba5676d29
Polkadoty <andrewdoty1235@gmail.com>
2024-10-26 23:57:25 -0500

    Updating main with speed insights.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { UniqueClassProvider } from '../contexts/UniqueClassContext'; // Import the provider
+import { UniqueClassProvider } from '../contexts/UniqueClassContext';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <UniqueClassProvider>
             <Component {...pageProps} />
             <Analytics />
+            <SpeedInsights />
           </UniqueClassProvider>
         </TooltipProvider>
       </ThemeProvider>
