c4866bdc25419ff25d78b8c10df045d2b289ed8b
Polkadoty <andrewdoty1235@gmail.com>
2024-11-08 02:39:58 -0600

    Updating the throttle.

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,10 +1,10 @@
-export function throttle<T extends (...args: any[]) => any>(
+export function throttle<T extends (...args: unknown[]) => unknown>(
   func: T,
   limit: number
 ): (...args: Parameters<T>) => void {
   let inThrottle: boolean;
   
-  return function(this: any, ...args: Parameters<T>) {
+  return function(this: unknown, ...args: Parameters<T>) {
     if (!inThrottle) {
       func.apply(this, args);
       inThrottle = true;
