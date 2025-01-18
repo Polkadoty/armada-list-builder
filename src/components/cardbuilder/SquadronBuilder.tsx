f767860d5d7cc398761e0c01c6b9beabb8baf12f
Polkadoty <andrewdoty1235@gmail.com>
2025-01-18 15:51:33 -0600

    Updating the squadronbuilder component.

--- src/components/cardbuilder/SquadronBuilder.tsx
+++ src/components/cardbuilder/SquadronBuilder.tsx
@@ -335,6 +335,7 @@ export function SquadronBuilder({ onBack }: Omit<SquadronBuilderProps, 'userId'>
             <div className="space-y-2">
               <Label>First Defense Token</Label>
               <Select
+              // eslint-disable-next-line no-unused-vars
                 value={Object.entries(formData.tokens).find(([key, value]) => value === 1)?.[0]?.replace('def_', '') || ''}
                 onValueChange={(value: DefenseTokenType) => {
                   const newTokens = { ...formData.tokens };
@@ -364,6 +365,7 @@ export function SquadronBuilder({ onBack }: Omit<SquadronBuilderProps, 'userId'>
               <div className="space-y-2">
                 <Label>Second Defense Token</Label>
                 <Select
+                // eslint-disable-next-line no-unused-vars
                   value={Object.entries(formData.tokens).find(([key, value]) => value === 2)?.[0] || ''}
                   onValueChange={(value: DefenseTokenType) => {
                     const newTokens = { ...formData.tokens };
