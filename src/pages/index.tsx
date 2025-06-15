eea9afb1fad3751df0a8b64e0087fb751cbe273a
Polkadoty <andrewdoty1235@gmail.com>
2025-06-15 03:59:32 -0500

    Updating to fix this compile error.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -158,7 +158,7 @@ export default function Home() {
           {/* Battle for Naboo Campaign Information */}
           <div className="mb-6 p-4 bg-blue-100/80 dark:bg-blue-900/30 border border-blue-200 dark:border-blue-700 rounded-lg backdrop-blur-md">
             <p className="text-sm text-blue-800 dark:text-blue-200 text-center">
-                              Join the <b>Battle for Naboo</b> event by making your fleet using the &quot;Battle for Naboo&quot; gamemode. Select it in the top right corner in the "plus" menu, and check out the status menu here:{' '}
+                                                             Join the <b>Battle for Naboo</b> event by making your fleet using the &quot;Battle for Naboo&quot; gamemode. Select it in the top right corner in the &quot;plus&quot; menu, and check out the status menu here:{' '}
               <a 
                 href="https://www.armada.community/events/battle-for-naboo" 
                 target="_blank" 
